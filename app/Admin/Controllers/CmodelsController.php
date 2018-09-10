9e1074c9604b029c27376e840237920189c03a9a
rambo <15019207291@163.com>
2018-09-10 17:29:37 +0800

    “车型管理”添加购车方案

--- app/Admin/Controllers/CmodelsController.php
+++ app/Admin/Controllers/CmodelsController.php
@@ -133,6 +133,163 @@ class CmodelsController extends Controller
             $form->text('zw', '座位数');
             $form->select('stype_id', '所属类型')->options(Stype::getSeletOptions());
 
+            $form->divide();
+            $form->html('', $label = '（全款）');
+
+            $form->radio('one_dszzrx_status', '（第三者责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+            $form->radio('one_dszzrx_default', '（第三者责任险）默认值')
+                    ->options([1 => '50万', 2 => '100万'])
+                    ->default(1);
+
+            $form->text('one_clssx', '（车辆损失险）金额');
+            $form->radio('one_clssx_status', '（车辆损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('one_qcdqx', '（全车盗抢险）金额');
+            $form->radio('one_qcdqx_status', '（全车盗抢险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->text('one_blddpsx', '（玻璃单独破碎险）金额');
+            $form->radio('one_blddpsx_status', '（玻璃单独破碎险）是否必选')
+                ->options([0 => '否', 1 => '是'])
+                ->default(0);
+
+            $form->radio('one_csryzrx_status', '（车上人员责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+            $form->radio('one_csryzrx_default', '（车上人员责任险）默认值')
+                    ->options([1 => '1万/座', 2 => '2万/座', 3 => '5万/座'])
+                    ->default(2);
+
+            $form->text('one_bjmptyx', '（不计免赔特约险）金额');
+            $form->radio('one_bjmptyx_status', '（不计免赔特约险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('one_wfzddsf', '（无法找到第三方）金额');
+            $form->radio('one_wfzddsf_status', '（无法找到第三方）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('one_zrssx', '（自然损失险）金额');
+            $form->radio('one_zrssx_status', '（自然损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->text('one_clsshuix', '（车辆涉水险）金额');
+            $form->radio('one_clsshuix_status', '（车辆涉水险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+
+            $form->divide();
+            $form->html('', $label = '（贷款）');
+
+            $form->radio('two_dszzrx_status', '（第三者责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+            $form->radio('two_dszzrx_default', '（第三者责任险）默认值')
+                    ->options([1 => '50万', 2 => '100万'])
+                    ->default(1);
+
+            $form->text('two_clssx', '（车辆损失险）金额');
+            $form->radio('two_clssx_status', '（车辆损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('two_qcdqx', '（全车盗抢险）金额');
+            $form->radio('two_qcdqx_status', '（全车盗抢险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('two_blddpsx', '（玻璃单独破碎险）金额');
+            $form->radio('two_blddpsx_status', '（玻璃单独破碎险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->radio('two_csryzrx_status', '（车上人员责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+            $form->radio('two_csryzrx_default', '（车上人员责任险）默认值')
+                    ->options([1 => '1万/座', 2 => '2万/座', 3 => '5万/座'])
+                    ->default(2);
+
+            $form->text('two_bjmptyx', '（不计免赔特约险）金额');
+            $form->radio('two_bjmptyx_status', '（不计免赔特约险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('two_wfzddsf', '（无法找到第三方）金额');
+            $form->radio('two_wfzddsf_status', '（无法找到第三方）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('two_zrssx', '（自然损失险）金额');
+            $form->radio('two_zrssx_status', '（自然损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->text('two_clsshuix', '（车辆涉水险）金额');
+            $form->radio('two_clsshuix_status', '（车辆涉水险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->divide();
+            $form->html('', $label = '（低首付/零首付）');
+
+            $form->radio('three_dszzrx_status', '（第三者责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+            $form->radio('three_dszzrx_default', '（第三者责任险）默认值')
+                    ->options([1 => '50万', 2 => '100万'])
+                    ->default(1);
+
+            $form->text('three_clssx', '（车辆损失险）金额');
+            $form->radio('three_clssx_status', '（车辆损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('three_qcdqx', '（全车盗抢险）金额');
+            $form->radio('three_qcdqx_status', '（全车盗抢险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('three_blddpsx', '（玻璃单独破碎险）金额');
+            $form->radio('three_blddpsx_status', '（玻璃单独破碎险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->radio('three_csryzrx_status', '（车上人员责任险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+            $form->radio('three_csryzrx_default', '（车上人员责任险）默认值')
+                    ->options([1 => '1万/座', 2 => '2万/座', 3 => '5万/座'])
+                    ->default(2);
+
+            $form->text('three_bjmptyx', '（不计免赔特约险）金额');
+            $form->radio('three_bjmptyx_status', '（不计免赔特约险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('three_wfzddsf', '（无法找到第三方）金额');
+            $form->radio('three_wfzddsf_status', '（无法找到第三方）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(1);
+
+            $form->text('three_zrssx', '（自然损失险）金额');
+            $form->radio('three_zrssx_status', '（自然损失险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
+            $form->text('three_clsshuix', '（车辆涉水险）金额');
+            $form->radio('three_clsshuix_status', '（车辆涉水险）是否必选')
+                    ->options([0 => '否', 1 => '是'])
+                    ->default(0);
+
             $form->display('created_at', 'Created At');
             $form->display('updated_at', 'Updated At');
         });
