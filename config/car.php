20ec765d1a80fa5aa86082603caeea465213b0f3
wangsz <15019207291@163.com>
2018-10-12 17:08:07 +0800

    完善订单管理

---
+++ config/car.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the overtrue/laravel-wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+return [
+    //提成百分比
+    'level1_percent' => 0.35,
+    'level2_percent' => 0.4,
+    'level3_percent' => 0.45,
+    'level4_percent' => 0.5,
+
+    //销售目标
+    'level1_sale_goal' => 1,
+    'level2_sale_goal' => 2,
+    'level3_sale_goal' => 4,
+    'level4_sale_goal' => 6,
+
+    //最大星星数量
+    'level1_star_max' => 2,
+    'level2_star_max' => 3,
+    'level3_star_max' => 4,
+    'level4_star_max' => 5,
+];
